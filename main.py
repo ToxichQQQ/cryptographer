663937315989ba835d72be33888b7571c57cf727
ToxichQQQ <storozhenko.dev@gmail.com>
2022-04-11 23:20:53 +0300

    Added UI

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 import hashlib
+from tkinter import *
+
+root = Tk()
+
+def createHash():
+   text = input.get()
+
+   text_sha1 = hashlib.sha1(text.encode('utf-8'))
+   result = text_sha1.hexdigest()
+
+   finalText['text'] = f'{result}'
 
 
-text = input('Ввведите сообщение для шифрования: ')
+root['bg'] = '#fff'
+root.title('Hash Program')
+root.geometry('500x500')
 
-text_sha1 = hashlib.sha1(text.encode('utf-8'))
-result = text_sha1.hexdigest()
+root.resizable(width=False,height=False)
 
-print(result)
-input('Press ENTER to exit')
+canvas = Canvas(root,height = 500, width = 500)
+canvas.pack()
+
+frame = Frame(root)
+frame.place(rely=0.15,relwidth = 1.0,relheight = 0.25)
+
+frameBottom = Frame(root)
+frameBottom.place(rely=0.55,relwidth = 1.0,relheight = 0.3)
+
+title = Label(frame,text = 'Введите текст',font=32)
+title.pack()
+
+input = Entry(frame,bg='white',font=32)
+input.pack()
+
+btn = Button(frame, text = 'Создать хеш',bg='#fff',command=createHash)
+btn.pack()
+
+
+title = Label(frameBottom,text = 'Хеш',font=32)
+title.pack()
+
+finalText = Label(frameBottom,font=32)
+finalText.pack()
+
+
+
+
+root.mainloop()
+
+
